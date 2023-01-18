47e9cc642988c71cdff2de83ec154da5b8337616
yosikari <yosikari@gmail.com>
2023-01-18 21:00:56 +0200

    start work on task details side page

--- frontend/src/cmps/task-details.jsx
+++ frontend/src/cmps/task-details.jsx
@@ -1,6 +1,6 @@
-export function TaskDetails() {
-return <section className='task-details'>
-<h1>TaskDetails</h1>
+export function TaskDetails({ task, isOpenDetails }) {
+    return <section className='task-details' style={{ right: `${isOpenDetails ? 10 : 20}vw` }}>
+        {/* <h1>TaskDetails</h1> */}
 
-</section>
+    </section>
 }
