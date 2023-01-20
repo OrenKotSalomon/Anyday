b50da2432822d9235a2213685e59a3174bfc9bee
OrenKotSalomon <orensalomonkot@gmail.com>
2023-01-20 14:14:08 +0200

    Added styling to comment popup

--- frontend/src/cmps/task-preview.jsx
+++ frontend/src/cmps/task-preview.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { TaskDetails } from "./task-details";
 
-import { MenuButton, Menu, MenuItem, Icon, EditableHeading } from 'monday-ui-react-core'
-import { Open, Duplicate, Delete, Bolt, AddUpdate } from 'monday-ui-react-core/icons'
+import { MenuButton, Menu, MenuItem, Icon, EditableHeading, Counter } from 'monday-ui-react-core'
+import { Open, Duplicate, Delete, Bolt, AddUpdate, Update } from 'monday-ui-react-core/icons'
 import { updateTask } from "../store/board.actions";
 import { DELETE_TASK, DUPLICATE_TASK } from "../services/board.service.local";
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service";
@@ -58,17 +58,21 @@ export function TaskPreview({ task, board, group }) {
                     <input className='row-checkbox' type="checkbox" />
                 </div>
 
-                <div className="task-name-cell">
+                <div className="task-name-cell" >
                     <EditableHeading className='task-title' type={EditableHeading.types.h5} value={task.title} />
                     {/* <button onClick={() => setIsOpenDetails(!isOpenDetails)} className="open-item-page-btn">
                         <Icon iconType={Icon.type.SVG} icon={Open} iconLabel="Task Details" iconSize={16} /><span>Open</span>
                     </button> */}
                 </div>
-                {/* <span>{Array.isArray(task.comments) && (task.comments.length !== 0) && task.comments.length}</span> */}
                 <div className="msg-btn-container">
 
-                    <button className="msg-btn">
-                        <Icon SVG="AddUpdate" iconType={Icon.type.SVG} icon={AddUpdate} iconLabel="Task Details" iconSize={24} /><span></span>
+                    <button className="msg-btn" style={task.comments ? { paddingRight: '0px' } : { paddingRight: '16px', paddingLeft: '18px' }}>
+
+                        {!task.comments && <Icon SVG="AddUpdate" iconType={Icon.type.SVG} icon={AddUpdate} iconLabel="Task Details" iconSize={24} />}
+                        {task.comments && <div className="storybook-counter_position">
+                            <Icon icon={Update} iconSize={24} />
+                            <Counter count={task.comments.length} size={Counter.sizes.SMALL} className='counter-comments' />
+                        </div>}
                     </button>
                 </div>
 
