1e6470024c02908ac0e53d7584bb7006b4886ecb
yosikari <yosikari@gmail.com>
2023-01-25 17:49:43 +0200

    start with member picker

--- frontend/src/cmps/dynamicCmps/dynamic-modal.jsx
+++ frontend/src/cmps/dynamicCmps/dynamic-modal.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import { DATE_PICKER, LABEL_STATUS_PICKER, MEMEBER_PICKER, PRIORITY_PICKER, STATUS_PICKER, UPDATE_TASK_DATE, UPDATE_TASK_LABEL_STATUS, UPDATE_TASK_PRIORITY, UPDATE_TASK_STATUS } from "../../services/board.service.local"
 
 import { DialogContentContainer, DatePicker, Avatar, Search } from 'monday-ui-react-core'
@@ -6,10 +8,21 @@ import Harel from '../../assets/img/Harel.jpg'
 import Yossi from '../../assets/img/Yossi.jpg'
 import Oren from '../../assets/img/Oren.jpg'
 
+import { userService } from '../../services/user.service.js'
 
 import dayjs from "dayjs"
 
 export function DynamicModal({ cmp, setIsModalOpen, onUpdateTaskLabel }) {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        loadUsers()
+    }, [])
+
+    async function loadUsers() {
+        const users = await userService.getUsers()
+        setUsers(users)
+    }
+
     const optionsAvatar = [{
         value: "Harel",
         label: "Harel Natan",
@@ -84,25 +97,35 @@ export function DynamicModal({ cmp, setIsModalOpen, onUpdateTaskLabel }) {
                     {/* defaultValue={[optionsAvatar[0]]} */}
                     {/* here goes amount of users connected to board  */}
                     <DialogContentContainer className="monday-style-story-chips_search-bar">
+                    <div className='member-picker-user-container'>
                         <Search placeholder="Search names, positions, or a team" />
-
-                        <div className="monday-style-story-chips_lable">Suggested people</div>
-                        <div className="monday-style-story-chips_search">
-                            <div className="monday-style-story-chips_inline-container" key="cont-1">
-                                <Avatar size={Avatar.sizes.MEDIUM} src={Oren} type={Avatar.types.IMG} />
-                                <span className="monday-style-story-chips_name">
-                                    Oren
-                                </span>
-                            </div>
-                            <div className="monday-style-story-chips_inline-container" key="cont-2">
-                                <Avatar size={Avatar.sizes.MEDIUM} backgroundColor={Avatar.colors.DARK_PURPLE} text="LC" type={Avatar.types.TEXT} />
-
-                            </div>
-
+                        <div className='member-picker-suggested'>Suggested people</div>
+                        {users && users.map(user => <div key={user._id} className="member-picker-user">
+                            <div className='member-picker-fullname'>{user.fullname}</div>
+                            {user.imgUrl!==''?<img className="member-picker-img" src={`${user.imgUrl}`} alt="" />
+                            :<div className='member-picker-img'><span className='member-picker-name-char'>{user.fullname.charAt(0).toUpperCase()}</span></div>}
+                        </div>)}
                         </div>
                     </DialogContentContainer>
                 </div>
             </div>
+
+
+
+            {/* <div className="monday-style-story-chips_lable">Suggested people</div>
+<div className="monday-style-story-chips_search">
+    <div className="monday-style-story-chips_inline-container" key="cont-1">
+        <Avatar size={Avatar.sizes.MEDIUM} src={Oren} type={Avatar.types.IMG} />
+        <span className="monday-style-story-chips_name">
+            Oren
+        </span>
+    </div>
+    <div className="monday-style-story-chips_inline-container" key="cont-2">
+        <Avatar size={Avatar.sizes.MEDIUM} backgroundColor={Avatar.colors.DARK_PURPLE} text="LC" type={Avatar.types.TEXT} />
+
+    </div>
+
+</div> */}
 
         case DATE_PICKER:
 
--- frontend/src/pages/user-details.jsx
+++ frontend/src/pages/user-details.jsx
@@ -2,13 +2,14 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
+import { userService } from '../services/user.service.js'
 import { loadUser } from '../store/user.actions'
 import { store } from '../store/store'
 import { showSuccessMsg } from '../services/event-bus.service'
 import { socketService, SOCKET_EVENT_USER_UPDATED, SOCKET_EMIT_USER_WATCH } from '../services/socket.service'
 
 export function UserDetails() {
-
+  const loggedInUser = userService.getLoggedinUser()
   const params = useParams()
   const user = useSelector(storeState => storeState.userModule.watchedUser)
 
@@ -32,15 +33,19 @@ export function UserDetails() {
   return (
     <section className="user-details">
       <h1>User Details</h1>
-      {user && <div>
+      {loggedInUser && <div>
         <h3>
-          {user.fullname}
+          {loggedInUser.fullname}
         </h3>
+        <h2>email: {loggedInUser.email}</h2>
         {/* Demo for dynamic images: */}
-        <div className="user-img" style={{ backgroundImage: `url('/img/u${0}.png')` }}>
-        </div>
+        {/* <div className="user-img" style={{ backgroundImage: `url('/img/u${0}.png')` }}>
+        </div> */}
+
+        <img src={`${loggedInUser.imgUrl}`} style={{ width: '50vw', objectFit:'contain'}} alt="" />
+
         <pre>
-          {JSON.stringify(user, null, 2)}
+          {JSON.stringify(loggedInUser, null, 2)}
         </pre>
       </div>}
     </section>
