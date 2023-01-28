4db91c52b64a82d807d88e8dcacb2fb0355e6f9e
OrenKotSalomon <orensalomonkot@gmail.com>
2023-01-28 22:41:36 +0200

    api check

--- frontend/src/services/http.service.js
+++ frontend/src/services/http.service.js
@@ -2,8 +2,8 @@ import Axios from 'axios'
 
 const BASE_URL = process.env.NODE_ENV === 'production'
     ? '/api/'
-    : '//localhost:3030/api/'
-
+    : '/api/'
+// : '//localhost:3030/api/'
 
 var axios = Axios.create({
     withCredentials: true
