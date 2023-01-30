cadf48383e23faf7ca0ce08b2fd0e27827907612
yosikari <yosikari@gmail.com>
2023-01-30 10:19:50 +0200

    db test

--- backend/api/board/board.service.js
+++ backend/api/board/board.service.js
@@ -61,17 +61,27 @@ async function add(board) {
 async function update(board) {
     try {
         const boardToSave = structuredClone(board)
-        delete boardToSave._id
+        let tempBoardToSave = {
+            archivedAt: board.archivedAt,
+            cmpsOrder: board.cmpsOrder,
+            groups: board.groups,
+            isStarred: board.isStarred,
+            labelStatuses: board.labelStatuses,
+            owner: board.owner,
+            priorities: board.priorities,
+            statuses: board.statuses,
+            title: board.title,
+        }
+
+        // delete board._id
         // console.log('boardToSave:', boardToSave)
-        // console.log('boardToSave:', boardToSave)
-        // console.log('board:', board)
         // console.log('boardToSave:', boardToSave)
         // console.log('board:', board)
         const collection = await dbService.getCollection('board')
         // console.log('collectioncollectioncollectioncollection', collection);
-        const test = await collection.findOne({ _id: ObjectId(board._id) }, { $set: boardToSave })
+        const test = await collection.findOne({ _id: ObjectId(board._id) }, { $set: tempBoardToSave })
         console.log('test:', test)
-        await collection.updateOne({ _id: ObjectId(board._id) }, { $set: boardToSave })
+        await collection.updateOne({ _id: ObjectId(board._id) }, { $set: tempBoardToSave })
         return board
     } catch (err) {
         logger.error(`cannot update board ${board._id}`, err)
